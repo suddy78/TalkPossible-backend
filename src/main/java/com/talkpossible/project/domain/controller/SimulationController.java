1f0ec79b0e7bc2256b507c21891e2c959ce1f11e
eujjong18 <eujjong18@naver.com>
2024-08-21 00:39:23 +0900

    [MERGE] 충돌 해결

--- src/main/java/com/talkpossible/project/domain/controller/SimulationController.java
+++ src/main/java/com/talkpossible/project/domain/controller/SimulationController.java
@@ -5,10 +5,7 @@ import com.talkpossible.project.domain.dto.simulations.response.UserSimulationRe
 import com.talkpossible.project.domain.service.SimulationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
