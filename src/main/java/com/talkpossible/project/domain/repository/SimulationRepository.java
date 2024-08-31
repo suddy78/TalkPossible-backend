e8384cf69e8e5d4c9f62db8795ab6e5f8440dae6
eujjong18 <eujjong18@naver.com>
2024-09-01 05:01:15 +0900

    [Fix #64]: 비관적 락 적용을 통한 Simulation 엔티티의 동시성 문제 해결 - @Lock 어노테이션을 사용하여 SimulationRepository에 비관적 락 적용 - 시뮬레이션 종료 후, 시뮬레이션 정보 및 발화 속도 업데이트 시 비관적 락 적용

--- src/main/java/com/talkpossible/project/domain/repository/SimulationRepository.java
+++ src/main/java/com/talkpossible/project/domain/repository/SimulationRepository.java
@@ -1,11 +1,21 @@
 package com.talkpossible.project.domain.repository;
 
 import com.talkpossible.project.domain.domain.Simulation;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SimulationRepository extends JpaRepository<Simulation, Long> {
 
     List<Simulation> findAllByPatientId(long patientdId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM Simulation s WHERE s.id = :id")
+    Optional<Simulation> findByIdWithLock(@Param("id") Long id);
+
 }
--- src/main/java/com/talkpossible/project/domain/service/MotionService.java
+++ src/main/java/com/talkpossible/project/domain/service/MotionService.java
@@ -7,11 +7,14 @@ import com.talkpossible.project.domain.dto.motion.request.UserMotionRequest;
 import com.talkpossible.project.domain.repository.MotionDetailRepository;
 import com.talkpossible.project.domain.repository.PatientRepository;
 import com.talkpossible.project.domain.repository.SimulationRepository;
+import com.talkpossible.project.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static com.talkpossible.project.global.exception.CustomErrorCode.SIMULATION_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -24,8 +27,11 @@ public class MotionService {
 
     @Transactional
     public void saveUserMotion(UserMotionRequest userMotionRequest) {
-        Simulation simulation = simulationRepository.findById(userMotionRequest.simulationId())
-                .orElseThrow(() -> new IllegalArgumentException("id에 해당하는 시뮬레이션 정보를 찾을 수 없습니다."));
+
+        //Simulation simulation = simulationRepository.findById(userMotionRequest.simulationId())
+        //        .orElseThrow(() -> new IllegalArgumentException("id에 해당하는 시뮬레이션 정보를 찾을 수 없습니다."));
+        Simulation simulation = simulationRepository.findByIdWithLock(userMotionRequest.simulationId())
+                .orElseThrow(() -> new CustomException(SIMULATION_NOT_FOUND));
 
         List<MotionDetail> motionDetails = userMotionRequest.motionList().stream().map(
                 motion -> MotionDetail.of(simulation, userMotionRequest, motion)).toList();
